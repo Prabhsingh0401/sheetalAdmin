4d5066c951be8e5af01130622194d1005a17f6e1
Devyansh Grover <devyansh.grover348@gmail.com>
2026-07-01 17:33:39 +0530

    minor changes

--- server/utils/ErrorResponse.js
+++ server/utils/ErrorResponse.js
@@ -1,8 +1,15 @@
-const ErrorResponse = (message, statusCode) => {
+function ErrorResponse(message, statusCode) {
+  if (!(this instanceof ErrorResponse)) {
+    return new ErrorResponse(message, statusCode);
+  }
   const error = new Error(message);
+  Object.setPrototypeOf(error, ErrorResponse.prototype);
   error.statusCode = statusCode;
   error.success = false;
   return error;
-};
+}
+
+ErrorResponse.prototype = Object.create(Error.prototype);
+ErrorResponse.prototype.constructor = ErrorResponse;
 
 export default ErrorResponse;
